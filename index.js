c304fee94e05201c7f89233e0e0fbc2e89ca1599
frkri <57220519+frkri@users.noreply.github.com>
2023-11-05 22:17:37 +0100

    add WebSocket server

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const port = new SerialPort({
   path: "COM3",
   baudRate: 115200,
 });
+console.log("READY");
 
 protobuf.load("common.proto").then(function (root) {
-  var AwesomeMessage = root.lookupType("common.SimpleMessage");
+  var AwesomeMessage = root.lookupType("croissantbit.Pong");
   port.on("readable", function () {
     var chunk = port.read(); // Reads whole buffer from serial port
     if (chunk == undefined) return;
@@ -18,11 +19,28 @@ protobuf.load("common.proto").then(function (root) {
 
     // Get packet id from chunk
     id = chunk.slice(0, 2);
-    console.log(id.readInt16LE(0));
-
-    // Remove id from chunk
     chunk = chunk.slice(2);
-    var message = AwesomeMessage.decode(chunk);
-    console.log(message);
+
+    switch (id.readInt16LE(0)) {
+      case 1:
+        console.log("Got Ping request");
+        break;
+      case 2:
+        console.log("Got Pong response");
+        var message = AwesomeMessage.decode(chunk);
+        console.log(message);
+        break;
+    }
+
+    console.log("====================================");
   });
+
+  setInterval(() => {
+    // Send ping packet
+    var ping = root.lookupType("croissantbit.Ping");
+    var buf = ping.encode().finish();
+    var packet = Buffer.concat([Buffer.from([1, 0]), buf]);
+    console.log("Sending:", packet);
+    port.write(packet);
+  }, 5000);
 });
